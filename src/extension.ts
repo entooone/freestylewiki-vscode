74c31a2c4d15105ac461c706c094f91ef55a196f
entooone <entooone@gmail.com>
2021-11-07 18:43:10 +0900

    フォーマッタ機能の追加をします
    Wasm を利用したフォーマッタ機能を実装しました。
    wasm_exec.js を extension.ts 内で使用するため、
    TypeScript と JavaScript を混在できる設定を追加しています。

--- src/extension.ts
+++ src/extension.ts
@@ -12,83 +12,135 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { rejects } from 'assert';
+import { resolve } from 'path/posix';
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import "./wasm_exec";
 
-class FSWDocumentsSymbolProvider {
-    private readonly pattern = /(^!{1,3}) *(.+)$/gm
-    private readonly tokenKind = 14
+declare const WebAssembly: any;
+declare const Go: any;
+declare const GoFSWiki: any;
 
-    private matchAll(text: string): RegExpExecArray[] {
-        const out: RegExpExecArray[] = [];
-        this.pattern.lastIndex = 0;
-        let match: RegExpExecArray | null = null;
-        while ((match = this.pattern.exec(text))) {
-            out.push(match);
-        }
-        return out;
-    }
+const go = new Go();
+let mod: any, inst: any
 
-    public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.DocumentSymbol[] {
-        const text = document.getText();
-        const matchedList = this.matchAll(text);
-        const symbols: vscode.DocumentSymbol[] = [];
-        let h1: vscode.DocumentSymbol | null = null;
-        let h2: vscode.DocumentSymbol | null = null;
-        let h3: vscode.DocumentSymbol | null = null;
-        matchedList.map((matched) => {
-            const type = matched[1];
-            const name = matched[2];
-            const kind = this.tokenKind;
-            const pos = document.positionAt(matched.index || 0);
-            const line = document.lineAt(pos.line);
-            const symbol = new vscode.DocumentSymbol(
-                `${type} ${name}`,
-                `L${pos.line + 1}`,
-                kind,
-                line.range,
-                line.range
-            );
-            symbol.children = [];
-            switch (type) {
-                case "!!!":
-                    h1 = symbol;
-                    symbols.push(h1);
-                    break;
-                case "!!":
-                    h2 = symbol;
-                    if (h1 !== null) {
-                        h1.children.push(h2);
-                    } else {
-                        symbols.push(h2);
-                    }
-                    break;
-                case "!":
-                    h3 = symbol;
-                    if (h1 !== null && h2 !== null) {
-                        const parent = h1.range.start.line > h2.range.start.line ? h1 : h2;
-                        parent.children.push(h3);
-                    } else if (h1 !== null) {
-                        h1.children.push(h3);
-                    } else if (h2 !== null) {
-                        h2.children.push(h3);
-                    } else {
-                        symbols.push(h3);
-                    }
-            }
-        });
-
-        return symbols;
-    }
+async function loadGOWasm(extensionUri: vscode.Uri) {
+	const wasmPath = vscode.Uri.joinPath(extensionUri, 'static', 'wasm.wasm').fsPath;
+	mod = await WebAssembly.compile(fs.readFileSync(wasmPath));
+	inst = await WebAssembly.instantiate(mod, go.importObject);
+	go.run(inst);
 }
 
-export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(
-        vscode.languages.registerDocumentSymbolProvider(
-            [
-                { language: 'fsw', pattern: '**/*.fsw' },
-                { language: 'fsw', scheme: 'file' },
-            ],
-            new FSWDocumentsSymbolProvider(),
-        )
-    );
+async function formatDocument(s: string): Promise<string> {
+	try {
+		return GoFSWiki.formatDocument(s);
+	} catch (e) {
+		inst = await WebAssembly.instantiate(mod, go.importObject);
+		go.run(inst);
+		return GoFSWiki.formatDocument(s);
+	}
+}
+
+class FSWDocumentsSymbolProvider implements vscode.DocumentSymbolProvider {
+	private readonly pattern = /(^!{1,3}) *(.+)$/gm;
+	private readonly tokenKind = 14;
+
+	private matchAll(text: string): RegExpExecArray[] {
+		const out: RegExpExecArray[] = [];
+		this.pattern.lastIndex = 0;
+		let match: RegExpExecArray | null = null;
+		while ((match = this.pattern.exec(text))) {
+			out.push(match);
+		}
+		return out;
+	}
+
+	public provideDocumentSymbols(document: vscode.TextDocument, _token: vscode.CancellationToken): vscode.DocumentSymbol[] {
+		const text = document.getText();
+		const matchedList = this.matchAll(text);
+		const symbols: vscode.DocumentSymbol[] = [];
+		let h1: vscode.DocumentSymbol | null = null;
+		let h2: vscode.DocumentSymbol | null = null;
+		let h3: vscode.DocumentSymbol | null = null;
+		matchedList.map((matched) => {
+			const type = matched[1];
+			const name = matched[2];
+			const kind = this.tokenKind;
+			const pos = document.positionAt(matched.index || 0);
+			const line = document.lineAt(pos.line);
+			const symbol = new vscode.DocumentSymbol(
+				`${type} ${name}`,
+				`L${pos.line + 1}`,
+				kind,
+				line.range,
+				line.range
+			);
+			symbol.children = [];
+			switch (type) {
+				case "!!!":
+					h1 = symbol;
+					symbols.push(h1);
+					break;
+				case "!!":
+					h2 = symbol;
+					if (h1 !== null) {
+						h1.children.push(h2);
+					} else {
+						symbols.push(h2);
+					}
+					break;
+				case "!":
+					h3 = symbol;
+					if (h1 !== null && h2 !== null) {
+						const parent = h1.range.start.line > h2.range.start.line ? h1 : h2;
+						parent.children.push(h3);
+					} else if (h1 !== null) {
+						h1.children.push(h3);
+					} else if (h2 !== null) {
+						h2.children.push(h3);
+					} else {
+						symbols.push(h3);
+					}
+			}
+		});
+		return symbols;
+	}
+}
+
+class FSWDocumentFormatter implements vscode.DocumentFormattingEditProvider {
+	public provideDocumentFormattingEdits(document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
+		return new Promise<vscode.TextEdit[]>((resolve, rejects) => {
+			formatDocument(document.getText()).then(result => {
+				const fileStart = new vscode.Position(0, 0);
+				const fileEnd = document.lineAt(document.lineCount - 1).range.end;
+				const textEdits: vscode.TextEdit[] = [
+					new vscode.TextEdit(new vscode.Range(fileStart, fileEnd), result),
+				];
+
+				return resolve(textEdits);
+			}).catch(e => {
+				rejects(e);
+			});
+		});
+	}
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+	const fswSelector = [
+		{ language: 'fsw', pattern: '**/*.fsw' },
+		{ language: 'fsw', scheme: 'file' },
+	];
+
+	await loadGOWasm(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.languages.registerDocumentSymbolProvider(
+			fswSelector, new FSWDocumentsSymbolProvider(),
+		)
+	);
+	context.subscriptions.push(
+		vscode.languages.registerDocumentFormattingEditProvider(
+			fswSelector, new FSWDocumentFormatter())
+	);
 }
